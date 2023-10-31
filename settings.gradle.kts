bd723967caf01f706875366fd96ea7bb50e81bd0
unknown <julsie157@alu.centroafuera.es>
2023-10-31 09:54:13 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "APPTASK05"
+include(":app")
+ 
